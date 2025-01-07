0b843c05a60edca9f7466de3a5141055f03e9681
Shree Chand <shree.chand@hostbooks.com>
2025-01-07 19:30:22 +0530

    add table data

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  formData: any[] = [];
 
+  saveData(a: any) {
+    this.formData.push(a);
+    // console.log(a.firstName);
+  }
 }
