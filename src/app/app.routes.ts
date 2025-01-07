51a7ff23e5e635fc9a5b9f056f0510340965efc3
Shree Chand <shree.chand@hostbooks.com>
2025-01-07 13:10:55 +0530

    add table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { TableComponent } from './table/table.component';
 
 export const routes: Routes = [
     { path:'about', component:AboutComponent},
-    { path: 'contact', component:ContactComponent}
+    { path:'table', component:TableComponent}
 ];
--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactComponent } from './contact.component';
+import { TableComponent } from './table.component';
 
-describe('ContactComponent', () => {
-  let component: ContactComponent;
-  let fixture: ComponentFixture<ContactComponent>;
+describe('TableComponent', () => {
+  let component: TableComponent;
+  let fixture: ComponentFixture<TableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactComponent]
+      imports: [TableComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(ContactComponent);
+    fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
